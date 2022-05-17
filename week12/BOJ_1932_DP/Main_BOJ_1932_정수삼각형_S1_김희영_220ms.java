e4a2ce015f2ba7f6d65c1f1b9935d7416c7dec14
ahwl8240 <85783443+ahwl8240@users.noreply.github.com>
2022-05-17 20:33:13 +0900

    Update and rename Main_BOJ_1932_정수삼각형_S1_김희영_232ms.java to Main_BOJ_1932_정수삼각형_S1_김희영_220ms.java

--- week12/BOJ_1932_DP/Main_BOJ_1932_정수삼각형_S1_김희영_220ms.java
+++ week12/BOJ_1932_DP/Main_BOJ_1932_정수삼각형_S1_김희영_220ms.java
@@ -20,7 +20,7 @@ import java.util.StringTokenizer;
  * 그러나 계산의 편의를 위해 memo[i - 1][j - 1]의 관점에서
  * memo[i][j]와 memo[i][j + 1]을 갱신하는 방향으로 작성
  */
-public class Main_BOJ_1932_정수삼각형_S1_김희영_232ms {
+public class Main_BOJ_1932_정수삼각형_S1_김희영_220ms {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -36,19 +36,18 @@ public class Main_BOJ_1932_정수삼각형_S1_김희영_232ms {
 		}
 
 		int[][] memo = new int[N][N];
-		memo[0][0] = arr[0][0];
+		memo[0][0] = arr[0][0]; // 초기화
 
 		for (int i = 1; i < N; i++) {
 			for (int j = 0; j < i; j++) {
 				int tmp = memo[i - 1][j] + arr[i][j];
 				memo[i][j] = Math.max(memo[i][j], tmp);
 
-				tmp = memo[i - 1][j] + arr[i][j + 1];
-				memo[i][j + 1] = Math.max(memo[i][j + 1], tmp);
+				memo[i][j + 1] = memo[i - 1][j] + arr[i][j + 1];
 			}
 		}
 
-		int max = memo[N - 1][0];
+		int max = memo[N - 1][0]; // 최대값 찾기
 		for (int i = 1; i < N; i++) {
 			if (memo[N - 1][i] > max)
 				max = memo[N - 1][i];
